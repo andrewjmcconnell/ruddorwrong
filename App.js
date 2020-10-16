a0029d3d06719b8a1b84a72c079e79a438fec4ad
andyjmcconn <amcconnell@thecitybase.com>
2020-10-16 16:51:25 -0500

    submitting cropped images works but model always wrong

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Platform, Text, View, Button, LogBox } from "react-native";
-// import RNFS from "react-native-fs";
+
+import "@expo/browser-polyfill";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  Button,
+  Image,
+  Dimensions
+} from "react-native";
+import * as FileSystem from "expo-file-system";
+import { decode as atob } from "base-64";
+import * as ImageManipulator from "expo-image-manipulator";
+
 import { Camera } from "expo-camera";
 import { Identifier } from "react-native-identifier";
 import * as Permissions from "expo-permissions";
@@ -8,6 +21,8 @@ import * as Permissions from "expo-permissions";
 import * as FaceDetector from "expo-face-detector";
 import * as tf from "@tensorflow/tfjs";
 import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tmImage from "@teachablemachine/image";
+
 import {
   cameraWithTensors,
   bundleResourceIO
@@ -17,6 +32,26 @@ import {
 import * as blazeface from "@tensorflow-models/blazeface";
 
 console.disableYellowBox = true;
+
+FileReader.prototype.readAsArrayBuffer = function(blob) {
+  console.log("BLOB", blob);
+  if (this.readyState === this.LOADING) throw new Error("InvalidStateError");
+  this._setReadyState(this.LOADING);
+  this._result = null;
+  this._error = null;
+  const fr = new FileReader();
+  fr.onloadend = () => {
+    const content = atob(
+      fr.result.substr("data:application/octet-stream;base64,".length)
+    );
+    const buffer = new ArrayBuffer(content.length);
+    const view = new Uint8Array(buffer);
+    view.set(Array.from(content).map(c => c.charCodeAt(0)));
+    this._result = buffer;
+    this._setReadyState(this.DONE);
+  };
+  fr.readAsDataURL(blob);
+};
 
 const TensorCamera = cameraWithTensors(Camera);
 // const modelJson = require('./model.js');
@@ -28,48 +63,45 @@ const TensorCamera = cameraWithTensors(Camera);
 
 export default () => {
   const [faces, setFaces] = useState([]);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [predictImage, setPredictImage] = useState(null);
+  const [croppedImages, setCroppedImages] = useState(null);
   // const [model, setModel] = useState(null);
-  const [mobilenetModel, setMobilenetModel] = useState(null);
+  const [model, setModel] = useState(null);
   const [frameworkReady, setFrameworkReady] = useState(false);
   const [
     permission,
     askForPermission
   ] = Permissions.usePermissions(Permissions.CAMERA, { ask: true });
 
-  const loadMobileNetModel = async () => {
-    console.log("loading mobilenet");
+  const loadModel = async () => {
+    console.log("loading model");
     // const model = await mobilenet.load({
     //   version: 1,
     //   modelUrl: "./model.json"
     // });
     // const model = await tf.loadLayersModel(
     //   bundleResourceIO(modelJson, modelWeights));
-    const model = await tf.loadLayersModel(
-      "https://teachablemachine.withgoogle.com/models/lNj2ao8Pz/model.json"
+    // const tfModel = await tf.loadLayersModel(
+    //   "https://teachablemachine.withgoogle.com/models/lNj2ao8Pz/model.json"
+    // );
+    const url = "https://teachablemachine.withgoogle.com/models/lNj2ao8Pz";
+    const tfModel = await tmImage.load(
+      url + "/model.json",
+      url + "/metadata.json"
     );
     console.log("SUMMARY");
-    console.log(model.summary());
+    // console.log(tfModel.summary());
     console.log("END SUMMARY");
-    return model;
+    return tfModel;
   };
 
   useEffect(() => {
     if (!frameworkReady) {
       (async () => {
-        //check permissions
-        // const { status } = await Camera.requestPermissionsAsync();
-        // console.log(`permissions status: ${status}`);
-        // setHasPermission(status === "granted");
-
-        //we must always wait for the Tensorflow API to be ready before any TF operation...
         await tf.ready();
-        setMobilenetModel(await loadMobileNetModel());
-        // const blazeModel = await blazeface.load();
-        // setModel(blazeModel);
+        setModel(await loadModel());
         console.log("TF READY!");
-
-        //load the mobilenet model and save it in state
-        // setMobilenetModel(await loadMobileNetModel());
 
         setFrameworkReady(true);
       })();
@@ -105,21 +137,76 @@ export default () => {
       return;
     }
 
-    console.log("tensor", tensor);
-    console.log("tensor shape", tensor.shape);
+    // console.log("tensor", tensor);
+    // console.log("tensor shape", tensor.shape);
 
     //topk set to 1
-    const prediction = await mobilenetModel.predict(tensor, { verbose: true });
-    console.log(`prediction: ${JSON.stringify(prediction)}`);
+    const prediction = await model.predict(tensor);
+    // console.log(`prediction: ${JSON.stringify(prediction)}`);
 
     if (!prediction || prediction.length === 0) {
       return;
     }
 
-    console.log("PREDICTION", prediction);
+    const data = await prediction.data();
+    console.log("PREDICTION", data[0]);
+    tf.dispose([tensor]);
+  };
+
+  const predict = async (data, faceProps) => {
+    console.log("faces", data.uri);
+    if (faceProps.length == 0) {
+      return;
+    }
+
+    const scale = data.height / Dimensions.get("window").height;
+
+    // create a cropped image for each face
+    let croppedimgs = [];
+    let _ = await faceProps.map(async face => {
+      // console.log("face", face);
+      const crop = await ImageManipulator.manipulateAsync(
+        data.uri,
+        [
+          {
+            crop: {
+              originX: scale * face.bounds.origin.x,
+              originY: scale * face.bounds.origin.y,
+              width: scale * face.bounds.size.width,
+              height: scale * face.bounds.size.height
+            }
+          },
+          {
+            resize: {
+              width: 224,
+              height: 224
+            }
+          }
+        ],
+        { compress: 1, format: ImageManipulator.SaveFormat.JPEG }
+      );
+      console.log("crop", crop.uri);
+      croppedimgs.push(crop);
+      const prediction = await model.predict(crop);
+      console.log(`prediction: ${JSON.stringify(prediction)}`);
+
+      return crop;
+    });
+    setCroppedImages(croppedimgs);
+
+    // const prediction = await model.predict(predictImage);
+    // // console.log(`prediction: ${JSON.stringify(prediction)}`);
+
+    // if (!prediction || prediction.length === 0) {
+    //   return;
+    // }
+
+    // const data = await prediction.data();
+    // console.log("PREDICTION", data[0]);
   };
 
   const handleCameraStream = imageAsTensors => {
+    // console.log("imageAsTensors", imageAsTensors);
     const loop = async () => {
       const imgTensor = tf.tidy(() => {
         let imgTensor = imageAsTensors.next().value;
@@ -127,25 +214,40 @@ export default () => {
         imgTensor = imgTensor.expandDims(0);
         return imgTensor;
       });
-
-      // const nextImageTensor = await imageAsTensors.next().value;
-      await getPrediction(
-        imageAsTensors.next().value.reshape([null, 224, 224, 3])
-      );
-      // console.log(nextImageTensor);
-      // console.log(nextImageTensor);
-      // const predictions = await model.estimateFaces(nextImageTensor);
-      // console.log("predictions", predictions);
-      // await setFaces(predictions);
-      // console.log("predictions", predictions);
-      // await getPrediction(nextImageTensor);
+      await getPrediction(imgTensor);
       requestAnimationFrameId = requestAnimationFrame(loop);
     };
     loop();
   };
 
   return (
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      onTouchEnd={() => {
+        if (cameraRef) {
+          // console.log(cameraRef);
+          const takePic = async () => {
+            cameraRef.pausePreview();
+            const facesSnapshot = faces;
+            const data = await cameraRef.takePictureAsync();
+            console.log("data", data);
+            setPredictImage(data.uri);
+            predict(data, facesSnapshot);
+          };
+          takePic();
+        }
+      }}
+    >
+      {!!croppedImages &&
+        croppedImages.length > 0 &&
+        croppedImages.map(img => (
+          <Image
+            style={{ width: img.width, height: img.height }}
+            source={{
+              uri: img.uri
+            }}
+          />
+        ))}
       {!!faces &&
         faces.length > 0 &&
         faces.map(face => {
@@ -165,7 +267,10 @@ export default () => {
             />
           );
         })}
-      <TensorCamera
+      <Camera
+        ref={ref => {
+          setCameraRef(ref);
+        }}
         style={{
           position: "absolute",
           width: "100%",
@@ -184,14 +289,14 @@ export default () => {
           setFaces(face.faces);
         }}
         // Tensor props
-        cameraTextureHeight={textureDims.height}
-        cameraTextureWidth={textureDims.width}
-        resizeHeight={200}
-        resizeWidth={152}
-        resizeDepth={3}
-        onReady={handleCameraStream}
-        autorender={false}
-      ></TensorCamera>
+        // cameraTextureHeight={textureDims.height}
+        // cameraTextureWidth={textureDims.width}
+        // resizeHeight={224}
+        // resizeWidth={224}
+        // resizeDepth={3}
+        // onReady={handleCameraStream}
+        // autorender={false}
+      ></Camera>
     </View>
   );
 };
